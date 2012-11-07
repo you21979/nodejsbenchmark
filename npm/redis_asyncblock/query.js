61abd389cb5c0cc39cdd65c0c29292c63d9f599c
you21979 <you2197901@gmail.com>
2012-11-07 13:04:41 +0900

    add pattern

--- npm/redis_asyncblock/query.js
+++ npm/redis_asyncblock/query.js
@@ -9,6 +9,24 @@ Query.prototype = {
     query : function(key, val){
         this.q_.push({key:key, val:val});
     },
+    exec0 : function(callback){
+        var self = this;
+        asyncblock(function(flow) {
+            flow.errorCallback = function(e){//例外処理はここにくる
+                console.log(e.stack);
+                self.q_.length = 0;
+            };
+            var data = '';
+            var result = null;
+            while(self.q_.length > 0){
+                data = self.q_.shift();
+                self.conn_.set(data.key, data.val, flow.add());
+                result = flow.wait();
+                //console.log(result);
+            }
+            callback();
+        });
+    },
     exec1 : function(callback){
         var self = this;
         asyncblock(function(flow) {

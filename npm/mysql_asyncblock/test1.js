76fc51500f2ac292cd120af0ce3a268c26916ff5
you21979 <you2197901@gmail.com>
2012-11-07 13:27:58 +0900

    mod

--- npm/mysql_asyncblock/test1.js
+++ npm/mysql_asyncblock/test1.js
@@ -2,22 +2,59 @@ var asyncblock = require('asyncblock');
 var mysql = require('mysql');
 var Query = require('./query');
 
-var mcl = mysql.createConnection({
+
+function dataset(q){
+    //q.query('begin');
+    for(var i=0;i<1000;++i){
+        q.query('insert into test1(id,rnd,str) values(?,?,?);', [i, Math.random()*100000|0, 'test']);
+    }
+    //q.query('commit');
+}
+
+var config = {
   host : '127.0.0.1',
   user : 'root',
   password : '',
   database : 'test',
-});
+};
 
-var q = new Query(mcl);
-//q.query('begin');
-for(var i=0;i<10000;++i){
-    q.query('insert into test1(id,rnd,str) values(?,?,?);', [i, Math.random()*100000|0, 'test']);
+[function(){
+    var TID = 'QUERY0';
+    var mcl = mysql.createConnection(config);
+    mcl.connect();
+    var q = new Query(mcl);
+    dataset(q);
+    console.log(TID);
+    console.time(TID);
+    q.exec0(function(){
+        console.timeEnd(TID);
+        mcl.end();
+    });
+},function(){
+    var TID = 'QUERY1';
+    var mcl = mysql.createConnection(config);
+    mcl.connect();
+    var q = new Query(mcl);
+    dataset(q);
+    console.log(TID);
+    console.time(TID);
+    q.exec1(function(){
+        console.timeEnd(TID);
+        mcl.end();
+    });
+},function(){
+    var TID = 'QUERY2';
+    var mcl = mysql.createConnection(config);
+    mcl.connect();
+    var q = new Query(mcl);
+    dataset(q);
+    console.log(TID);
+    console.time(TID);
+    q.exec2(function(){
+        console.timeEnd(TID);
+        mcl.end();
+    });
 }
-//q.query('commit');
-console.log('GO');
-console.time('QUERY');
-q.exec1(function(){
-    console.timeEnd('QUERY');
-    mcl.end();
+].forEach(function(fnc){
+    fnc();
 });

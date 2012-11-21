0170055ec7af42185fdd1381147747faf2924349
you21979 <you2197901@gmail.com>
2012-11-21 13:56:09 +0900

    add string_join

---
+++ v8/string_join.js
@@ -0,0 +1,63 @@
+var MAX = 1500000;
+[
+function(){
+    var TID = "join1";
+    console.time(TID);
+    for(var i = 0; i<MAX; ++i){
+        var o = [];
+        o.push('abcdefg');
+        o.push('abcdefg');
+        o.push('abcdefg');
+        o.push('abcdefg');
+        o.push('abcdefg');
+        o.push('abcdefg');
+        o.push('abcdefg');
+        o.push('abcdefg');
+        o.push('abcdefg');
+        o.push('abcdefg');
+        var x = o.join('');
+    }
+    console.timeEnd(TID);
+},
+function(){
+    var TID = "join2";
+    console.time(TID);
+    for(var i = 0; i<MAX; ++i){
+        var o = '';
+        o += 'abcdefg';
+        o += 'abcdefg';
+        o += 'abcdefg';
+        o += 'abcdefg';
+        o += 'abcdefg';
+        o += 'abcdefg';
+        o += 'abcdefg';
+        o += 'abcdefg';
+        o += 'abcdefg';
+        o += 'abcdefg';
+    }
+    console.timeEnd(TID);
+},
+function(){
+    var TID = "join3";
+    console.time(TID);
+    for(var i = 0; i<MAX; ++i){
+        var o = "";
+        o += "abcdefg";
+        o += "abcdefg";
+        o += "abcdefg";
+        o += "abcdefg";
+        o += "abcdefg";
+        o += "abcdefg";
+        o += "abcdefg";
+        o += "abcdefg";
+        o += "abcdefg";
+        o += "abcdefg";
+    }
+    console.timeEnd(TID);
+},
+function(){
+}
+].forEach(function(fnc){
+    fnc();
+});
+
